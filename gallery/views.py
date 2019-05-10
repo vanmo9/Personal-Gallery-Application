e6f88aaaf24ee1ac8f95b47f15c338469a81bff4
mohassan <vanmowha@gmail.com>
2019-05-10 12:45:57 +0300

    import render

--- gallery/views.py
+++ gallery/views.py
@@ -1,15 +1,7 @@
-from django.shortcuts import render 
+from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt 
+from django.shortcuts import render
 
 # Create your views here.
-def news_of_day(request):
-    date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+
--- photo/settings.py
+++ photo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
