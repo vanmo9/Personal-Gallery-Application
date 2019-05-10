487dc1e95eefc0c72d08dfaf2211f032b847edf8
mohassan <vanmowha@gmail.com>
2019-05-10 11:04:24 +0300

    first commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mohamed/Documents/core-ips/Personal-Photo-Gallery-Application/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
