748deb8ac5984d1deaf9d64f6bf4ede145321d73
mohassan <vanmowha@gmail.com>
2019-05-11 15:08:12 +0300

    added url patterns for search

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(          )
+    url(r'^$',views.photo,name='photoShoot'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
